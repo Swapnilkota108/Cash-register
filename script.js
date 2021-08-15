728219c5a46b66521a363945148033eaef4e898c
Swapnil kota <83860595+Swapnilkota108@users.noreply.github.com>
2021-08-15 14:23:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,37 +8,39 @@ const table = document.querySelector('.change');
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 table.style.display = 'none';
 
-checkButton.addEventListener('click',function validateBillandCashAmount(){
-hideMessage();    
-    if (billAmount.value > 0){
-        if(billAmount.value<=cashGiven.value){
-     const amountToBeReturned = cashGiven.value - billAmount.value;
+checkButton.addEventListener('click',function validateBillAmount(){
+    hideMessage();
+if(billAmount.value>0){
+if(billAmount.value<=cashGiven.value){
+    const amountToBeReturned = cashGiven.value - billAmount.value;
      calculateChange(amountToBeReturned);
      table.style.display = 'block';
-         }else {
-         showMessage('Do you want to wash plates?');
-         }
-    } else {
-       showMessage('Invalid Bill Amount');
+}
+else {
+    showMessage('Do you want to wash plates?');
     }
+} else {
+  showMessage('Invalid Bill Amount');
+}
+
+
+
+
+
 })
 
-
-
 function calculateChange(amountToBeReturned){
-   for(let i=0 ; i< availableNotes.length ; i++){
-       const numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
-       amountToBeReturned %= availableNotes[i];
-       noOfNotes[i].innerText = numberOfNotes;
-   }
+    for(let i=0 ; i< availableNotes.length ; i++){
+        const numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
+        amountToBeReturned %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }}
 
-}
-
-function showMessage(msg){
-message.style.display = 'block';
-message.innerText = msg;
-}
-
-function hideMessage(){
-message.style.display = 'none';
-}
+    function showMessage(msg){
+        message.style.display = 'block';
+        message.innerText = msg;
+        }
+        
+        function hideMessage(){
+        message.style.display = 'none';
+        }
